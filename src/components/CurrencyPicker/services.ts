63e87900d58e578d891045ba4f787da07c480840
Guilherme Bafica <gb.engcomp@hotmail.com>
2023-02-21 22:22:08 -0300

    Development of basic components (#3)

---
+++ src/components/CurrencyPicker/services.ts
@@ -0,0 +1,22 @@
+export const currencies = [
+  {
+    name: 'Brazilian Real',
+    flag: '',
+    abbreviation: 'BRL',
+  },
+  {
+    name: 'US Dollar',
+    flag: '',
+    abbreviation: 'USD',
+  },
+  {
+    name: 'Euro',
+    flag: '',
+    abbreviation: 'EUR',
+  },
+  {
+    name: 'British Pound',
+    flag: '',
+    abbreviation: 'GBP',
+  },
+]
